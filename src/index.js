db64496dbe00ea3bce4176e2874b90d6ba1b27e2
PortableProgrammer <nick@warnerheavyindustries.com>
2025-01-03 21:35:14 +0000

    Revert "Troubleshooting K3s: Move to `http.createServer` instead of `app.listen`"
    This reverts commit 063b5e001367f9a43ce15c21d863e351e301ffff.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const rateLimit = require("express-rate-limit");
 const path = require("node:path");
 const cookieParser = require("cookie-parser");
-const http = require("http");
 const app = express();
 const hasher = new Bun.CryptoHasher("sha256", "secret-key");
 const JWT_KEY = hasher.update(Math.random().toString()).digest("hex");
@@ -29,9 +28,7 @@ app.use(
 );
 app.use("/", routes);
 
-const port = process.env.LURKER_PORT || 3000;
-
-const server = http.createServer(app).listen(port, "0.0.0.0", () => {
-//const server = app.listen(port ? port : 3000, "0.0.0.0", () => {
+const port = process.env.LURKER_PORT;
+const server = app.listen(port ? port : 3000, "0.0.0.0", () => {
 	console.log("started on", server.address());
 });
